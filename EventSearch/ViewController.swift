8afcd43cedef5b5c0b81cacbcfdcd5ad6ebfefca
ryosukei <87470031+ryosukei@users.noreply.github.com>
2021-10-26 10:05:18 +0900

    Initial Commit

---
+++ EventSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventSearch
+//
+//  Created by 青野　凌介 on 2021/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
